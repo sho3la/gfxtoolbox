a710db71f162c375ad86b889f2b5d38403aabf8b
mohamedsh3lan <mohamed.shaalan@symbyo.com>
2024-10-22 18:27:37 +0300

    random not correct

--- examples/18_clouds_shader_example/main.cpp
+++ examples/18_clouds_shader_example/main.cpp
@@ -138,31 +138,47 @@ const char* fragmentShader =R"(
 
 		out vec4 FragColor;
 
-		float hash(float n)
+		// random/hash function
+		float hash( float n )
 		{
-			return fract(sin(n) * 43758.5453123);
+		  return fract(cos(n)*41415.92653);
 		}
 
-		float noise(vec3 x)
+		// 2d noise function
+		float noise( in vec2 x )
 		{
-			vec3 f = fract(x);
-			float n = dot(floor(x), vec3(1.0, 157.0, 113.0));
-			return mix(mix(mix(hash(n +   0.0), hash(n +   1.0), f.x),
-							mix(hash(n + 157.0), hash(n + 158.0), f.x), f.y),
-						mix(mix(hash(n + 113.0), hash(n + 114.0), f.x),
-							mix(hash(n + 270.0), hash(n + 271.0), f.x), f.y), f.z);
+		  vec2 p  = floor(x);
+		  vec2 f  = smoothstep(0.0, 1.0, fract(x));
+		  float n = p.x + p.y*57.0;
+
+		  return mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+			mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y);
 		}
 
-		const mat3 m = mat3(0.0, 1.60,  1.20, -1.6, 0.72, -0.96, -1.2, -0.96, 1.28);
-		float fbm(vec3 p)
+		// 3d noise function
+		float noise( in vec3 x )
 		{
-			float f = 0.0;
-			f += noise(p) / 2; p = m * p * 1.1;
-			f += noise(p) / 4; p = m * p * 1.2;
-			f += noise(p) / 6; p = m * p * 1.3;
-			f += noise(p) / 12; p = m * p * 1.4;
-			f += noise(p) / 24;
-			return f;
+		  vec3 p  = floor(x);
+		  vec3 f  = smoothstep(0.0, 1.0, fract(x));
+		  float n = p.x + p.y*57.0 + 113.0*p.z;
+
+		  return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+			mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y),
+			mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+			mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
+		}
+
+
+		mat3 m = mat3( 0.00,  1.60,  1.20, -1.60,  0.72, -0.96, -1.20, -0.96,  1.28 );
+
+		// Fractional Brownian motion
+		float fbm( vec3 p )
+		{
+		  float f = 0.5000*noise( p ); p = m*p*1.2;
+		  f += 0.2500*noise( p ); p = m*p*1.3;
+		  f += 0.1666*noise( p ); p = m*p*1.4;
+		  f += 0.0834*noise( p );
+		  return f;
 		}
 
 		vec3 skyColor( in vec3 rd )
@@ -173,22 +189,21 @@ const char* fragmentShader =R"(
 			rd.y = max(rd.y, 0.);
 
 			vec3 col = vec3(0.);
-			col += vec3(0.1, 0.1, 0.2) * (1. - exp(-rd.y * 8.0) ) * exp(-rd.y * 0.9); // Blue
-			col = mix(col*1.2, vec3(0.8),  1.-exp(yd*100.)); // Fog
-			col += vec3(1.0, 1.0, 1.0) * pow( max(dot(rd,moondir),0.), 15. ) * .3; // Moon
-			col += vec3(1.0, 1.0, 1.0) * pow(max(dot(rd, moondir),0.), 300.0) ;
+			// render sky
+			float t = pow(1.0-0.7*rd.y, 1.0);
+			col += vec3(.1, .2, .4)*(1.0-t);
+			// moon
+			col += min( vec3(2.0, 2.0, 2.0), vec3(2.0,2.0,2.0) * pow( max(dot(rd,moondir),0.), 350.0 )) * 0.3;
+			// moon haze
+			col += 0.6* vec3(0.8,0.9,1.0) * pow( max(dot(rd,moondir),0.), 6.0 );
 
-			// Cloud color
-			if (rd.y > 0.0)
-			{
-				// Only calculate for the upper half
-				float cirrus = 0.5;
-				float cloud_scale = 5.0;
-				float density = smoothstep(1.0 - cirrus, 1.0,  fbm(rd * cloud_scale));
-				vec3 cloudColor = vec3(1.0, 1.0, 1.0); // White clouds
+			// stars
+			vec3 stars = vec3(0.,0.,0.);
+			vec3 scol = clamp(vec3(1.2, 1.0, 0.8) * pow(noise(rd*120.), 120.) * 50. * (.5-pow(t,20.)), 0.0, 1.0);
+			scol += clamp(vec3(1.2, 1.0, 0.8) * pow(noise(rd*160.), 300.) * 40. * (.5-pow(t,20.)), 0.0, 1.0);
 
-				return mix(col, cloudColor, density); // Combine sky and cloud colors
-			}
+			stars = scol * (.3+.7*fbm(rd));
+			col += stars;
 
 			return col;
 		}
@@ -308,7 +323,7 @@ render()
 	gfx_backend->draw(gfx::GFX_Primitive::TRIANGLES_STRIP, gpu_mesh_id, 4);
 	glEnable(GL_DEPTH_TEST);
 
-	_draw_grid();
+	//_draw_grid();
 }
 
 void
