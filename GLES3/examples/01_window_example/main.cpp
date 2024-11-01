788fb0911fff8e314d35eb9aeb7f60ab148d7e98
mohamedsh3lan <mohamed.shaalan@symbyo.com>
2024-11-02 00:33:36 +0200

    cleanup

--- GLES3/examples/01_window_example/main.cpp
+++ GLES3/examples/01_window_example/main.cpp
@@ -13,8 +13,6 @@ render()
 {
 	gfx_backend->setClearColor(glm::vec4(0.0f, 0.67f, 0.9f, 1.0f));
 	gfx_backend->clearBuffer();
-
-	printf("outside \n");
 }
 
 int
@@ -29,66 +27,3 @@ main()
 
 	return 0;
 }
-
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//#include "glad/glad.h"
-//#include <GLFW/glfw3.h>
-//
-//#ifdef __EMSCRIPTEN__
-//#include <emscripten/emscripten.h>
-//#endif
-//
-//const GLuint WIDTH = 800, HEIGHT = 600;
-//
-//void
-//key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-//{
-//	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-//		glfwSetWindowShouldClose(window, GL_TRUE);
-//}
-//
-//void
-//render_frame(GLFWwindow* window)
-//{
-//	glfwPollEvents();
-//
-//	glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
-//	glClear(GL_COLOR_BUFFER_BIT);
-//
-//	glfwSwapBuffers(window);
-//}
-//
-//int
-//main(void)
-//{
-//	glfwInit();
-//
-//	// Set GLFW to use OpenGL ES 3.2
-//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
-//
-//	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GLES2 with GLFW", NULL, NULL);
-//	glfwMakeContextCurrent(window);
-//
-//	glfwSetKeyCallback(window, key_callback);
-//
-//	/* Load GLES */
-//	int gles_version = gladLoadGLES2Loader((GLADloadproc)glfwGetProcAddress);
-//
-//	printf("GLES %d.%d\n", GLVersion.major, GLVersion.minor);
-//
-//#ifdef __EMSCRIPTEN__
-//	emscripten_set_main_loop_arg((em_arg_callback_func)render_frame, window, 60, 1);
-//#else
-//	while (!glfwWindowShouldClose(window))
-//	{
-//		render_frame(window);
-//	}
-//#endif
-//
-//	return 0;
-//}
